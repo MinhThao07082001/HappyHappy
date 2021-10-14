64b79bea95de9f9b937af0a8344eb3e567f79fc4
MinhThao07082001 <63890568+MinhThao07082001@users.noreply.github.com>
2021-10-14 22:00:23 +0700

    Update MentorDAO.java

--- src/java/dal/MentorDAO.java
+++ src/java/dal/MentorDAO.java
@@ -5,6 +5,8 @@
  */
 package dal;
 
+import static java.lang.ProcessBuilder.Redirect.to;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import model.UserCommon;
 
@@ -12,14 +14,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import model.Major;
 import model.Mentor;
 import model.Subject;
-
-/**
- *
- * @author Admin
- */
 public class MentorDAO extends DBContext {
 
     public int updateImageAuthenMentor(int userID, String img1, String img2) {
@@ -48,60 +44,9 @@ public class MentorDAO extends DBContext {
         }
         return 0;
     }
-    public int createMentorPro(Mentor m) {
-        String sql = "insert into mentor (education,yearExperiment,intro) values(?,?,?) where userID=? ;\n" +
-                     "insert into major (subjectID) values (?) where mentorID=?;";
-        try {
-            PreparedStatement st = connection.prepareCall(sql);
-            Major mj=new Major();
-            st.setString(1, m.getEducation());
-            st.setInt(2, m.getYearExperiment());
-            st.setString(3, m.getIntro());
-            st.setInt(4, m.getMentorID());
-            st.setInt(5, mj.getSubjectID());
-            st.setInt(6, m.getMentorID());
-            return st.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return 0;
-    }
-    public int updateMentor(Mentor m) {
-        String sql = "UPDATE userCommon set "
-                + "      [name] = ?,"
-                + "      [dob] = ?,"
-                + "      [address] = ?,"
-                + "      [phone] = ?,"
-                + "      [imgAvt] = ?,"
-                + "      [description] = ?"
-                + "      where userID = ?;\n"; 
-//                     "UPDATE mentor set "
-//                + "      [education] = ?, "
-//                + "      [yearExperiment] = ?, "
-//                + "      [intro] ?, "
-//                + "      where userID = ?;";
-        try {
-            PreparedStatement st = connection.prepareStatement(sql);
-            st.setString(1, m.getName());
-            st.setString(2, m.getDob());
-            st.setString(3, m.getAddress());
-            st.setString(4, m.getPhone());
-            st.setString(5, m.getImgAvt());
-            st.setString(6, m.getDescription());
-            st.setInt(7, m.getMentorID());
-//            st.setString(8, m.getEducation());
-//            st.setInt(9, m.getYearExperiment());
-//            st.setString(10, m.getIntro());
-//            st.setInt(11, m.getMentorID());
-            return st.executeUpdate();
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        
-        return 0;
-    }
-    private int updateMentorAuthen(Mentor mentor) {
-        String sql = "UPDATE mentors set authen = 1 where mentorID = " + mentor.getMentorID();
+
+    public int updateMentorAuthen(int id) {
+        String sql = "UPDATE mentor set authen = 1 where userID = " +id;
         try {
             PreparedStatement ps = connection.prepareStatement(sql);
             return ps.executeUpdate();
@@ -146,12 +91,12 @@ public class MentorDAO extends DBContext {
         }
         return null;
     }
-    public Mentor getMentorByName(String name) {
-        String sql = "select name,email,imgAvt,education,yearExperiment,intro "
-                + "from mentor m join userCommon u on m.userID=u.userID where name = ?";
+
+    public Mentor getMentorById(int id) {
+        String sql = "SELECT * FROM userCommon u inner join mentor m on u.userID = m.userID where u.userID = ?";
         try {
             PreparedStatement st = connection.prepareCall(sql);
-            st.setString(1, name);
+            st.setInt(1, id);
             ResultSet rs = st.executeQuery();
             if (rs.next()) {
                 Mentor mentor = new Mentor();
@@ -174,6 +119,8 @@ public class MentorDAO extends DBContext {
                 mentor.setImgAuthen1(rs.getString(19));
                 mentor.setImgAuthen2(rs.getString(20));
                 mentor.setAuthen(rs.getInt(21));
+                MajorDAO md = new MajorDAO();
+                mentor.setListMajor(md.getListMajorByMentorID(id));
                 return mentor;
             }
         } catch (SQLException e) {
@@ -181,6 +128,7 @@ public class MentorDAO extends DBContext {
         }
         return null;
     }
+    
     public List<Mentor> getListMentor() {
         List<Mentor> mList = new ArrayList<>();
         String sql = "SELECT TOP 10 * FROM userCommon u inner join mentor m on u.userID = m.userID";
@@ -211,7 +159,46 @@ public class MentorDAO extends DBContext {
                 MajorDAO md = new MajorDAO();
                 List<Subject> sList = md.getListMajorByMentorID(mentor.getMentorID());
                 mentor.setListMajor(sList);
-                mList.add(mentor);  
+                mList.add(mentor);
+            }
+            return mList;
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public List<Mentor> getListMentorNotAuthen() {
+        List<Mentor> mList = new ArrayList<>();
+        String sql = "SELECT * FROM userCommon u inner join mentor m on u.userID = m.userID where authen = 0";
+        try {
+            PreparedStatement st = connection.prepareCall(sql);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Mentor mentor = new Mentor();
+                mentor.setMentorID(rs.getInt(1));
+                mentor.setName(rs.getString(2));
+                mentor.setPassword(rs.getString(3));
+                mentor.setEmail(rs.getString(4));
+                mentor.setDob(rs.getString(5));
+                mentor.setSex(rs.getInt(6));
+                mentor.setAddress(rs.getString(7));
+                mentor.setPhone(rs.getString(8));
+                mentor.setImgAvt(rs.getString(9));
+                mentor.setDescription(rs.getString(10));
+                mentor.setStatus(rs.getString(11));
+                mentor.setMoneyLeft(rs.getInt(12));
+                mentor.setCreateTime(rs.getString(13));
+                mentor.setEducation(rs.getString(16));
+                mentor.setYearExperiment(rs.getInt(17));
+                mentor.setIntro(rs.getString(18));
+                mentor.setImgAuthen1(rs.getString(19));
+                mentor.setImgAuthen2(rs.getString(20));
+                mentor.setAuthen(rs.getInt(21));
+                MajorDAO md = new MajorDAO();
+                List<Subject> sList = md.getListMajorByMentorID(mentor.getMentorID());
+                mentor.setListMajor(sList);
+                mList.add(mentor);
             }
             return mList;
         } catch (SQLException e) {
