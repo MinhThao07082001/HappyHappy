eb77a9e5f173c0ce1dea75eff59680ccade8a1e3
WacMac113 <unknowquest@yahoo.com>
2021-10-28 13:06:27 +0700

    28/10

--- src/java/dal/RateAndCommentDAO.java
+++ src/java/dal/RateAndCommentDAO.java
@@ -9,14 +9,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import model.Rating;
-import model.Request;
-
 /**
  *
  * @author WacMac
  */
 public class RateAndCommentDAO extends DBContext {
-    public int RateMentor(Rating rate){
+    public int insertRateMentor(Rating rate){
         String sql="insert into rating(rateAmount,rateDescription,menteeID,mentorID) values (?,?,?,?)"; 
         try{
             PreparedStatement st = connection.prepareStatement(sql);      
@@ -39,8 +37,7 @@ public class RateAndCommentDAO extends DBContext {
         st.setInt(2, id2);
         ResultSet rs = st.executeQuery();       
            if (rs.next()) {
-               return rs.getInt(1)>0;
-               
+               return rs.getInt(1)>0;     
             }
         }
         catch(SQLException e){
@@ -48,10 +45,57 @@ public class RateAndCommentDAO extends DBContext {
         }
             return false;  
     }
- 
     
+    public boolean checkRated(int id, int id2){
+        String sql = "select count(*) from Rating where menteeID=? and mentorID=?";
+        try{
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setInt(1, id);
+            st.setInt(2, id2);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                return rs.getInt(1)>0;
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e);
+        }
+        return false;
+    }
+    
+    
+    public float getRatebyMentorID(int ID) {
+        String sql="select  Format(AVG(cast(rateAmount as float)),'N1') from rating where mentorID=?";
+        try{
+             PreparedStatement st = connection.prepareStatement(sql);      
+            st.setInt(1, ID);
+            ResultSet rs = st.executeQuery();
+            if(rs.next()){
+                return  rs.getFloat(1);
+            }
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+    return 0;
+}
+    
+    public int updateRate(Rating rate){
+        String sql = "UPDATE Rating set rateAmount = ?,rateDescription = ?  where mentorID = ? and menteeID = ?";
+    try{
+        PreparedStatement st = connection.prepareStatement(sql);
+        st.setInt(1, rate.getRateAmount());
+        st.setString(2, rate.getRateDescription());
+        st.setInt(3, rate.getMentorID());
+        st.setInt(4, rate.getMenteeID());
+        return st.executeUpdate();
+    }catch(SQLException e){
+        System.out.println(e);
+    }
+    return 0;
+}   
     public static void main(String[] args) {
-        RateAndCommentDAO rate = new RateAndCommentDAO();       
-        System.out.println(rate.CheckLearned(3,2));
+        RateAndCommentDAO rate = new RateAndCommentDAO();
+        rate.insertRateMentor(new Rating(2, "Allahu",2,4));
     }
 }
