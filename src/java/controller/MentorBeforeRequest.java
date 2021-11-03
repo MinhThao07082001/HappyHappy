9f38d35dace7af223181b3c5ac8792de60b32d0a
thangavang1408 <puungok@gmail.com>
2021-11-03 11:34:41 +0700

    Update

---
+++ src/java/controller/MentorBeforeRequest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dal.MentorDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Mentor;
+
+/**
+ *
+ * @author Admin
+ */
+
+@MultipartConfig()
+@WebServlet(name = "MentorCV", urlPatterns = {"/user/mentor/updatecv"})
+public class MentorCV extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet MenteeEditProfile</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet MenteeEditProfile at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();  
+        MentorDAO tod = new MentorDAO();
+        String email = (String)session.getAttribute("email");
+        Mentor m = tod.getMentorByEmail(email);
+        request.setAttribute("mentor", m);
+        request.getRequestDispatcher("/user/mentor/mentor_update_cv.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("utf-8");
+        HttpSession session = request.getSession();
+        Mentor m = (Mentor) session.getAttribute("user");
+        MentorDAO md = new MentorDAO();
+        String edu = request.getParameter("education");
+        int yearE = Integer.parseInt(request.getParameter("yearExperiment"));
+        String intro = request.getParameter("intro");
+        m.setEducation(edu);
+        m.setYearExperiment(yearE);
+        m.setIntro(intro);
+        md.updateMentorCV(m);
+        session.setAttribute("mentor", m);
+        request.getRequestDispatcher("/user/mentor/mentor_update_cv.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
